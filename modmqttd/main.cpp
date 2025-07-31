4b1891335168c9d215fa414fa37348bfae2cf1b8
Nick <johndoe@example.com>
2025-07-31 20:44:57 +0300

    replace Boost.Program_options with getopt_long

--- modmqttd/main.cpp
+++ modmqttd/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-#include <boost/program_options.hpp>
-#include <boost/filesystem.hpp>
+#include <getopt.h>
+#include <filesystem>
 #include "libmodmqttsrv/common.hpp"
 #include "libmodmqttsrv/modmqtt.hpp"
 #include "config.hpp"
 #include "spdlog/spdlog.h"
 
-namespace args = boost::program_options;
 using namespace std;
 
 modmqttd::ModMqtt server;
@@ -16,28 +15,40 @@ modmqttd::ModMqtt server;
 int main(int ac, char* av[]) {
     std::string configPath;
     try {
-        args::options_description desc("Arguments");
+        modmqttd::Log::severity level = modmqttd::Log::severity::info;
 
         int logLevel;
 
-        desc.add_options()
-            ("help", "produce help message")
-            ("loglevel, l", args::value<int>(&logLevel), "setup logging: 0 off, 1-6 sets loglevel, higher is more verbose")
-            ("config, c", args::value<string>(&configPath), "path to configuration file")
-        ;
+        struct option cmdOptions[] = {
+            {"help", no_argument, nullptr, 'h'},
+            {"loglevel", required_argument, nullptr, 'l'},
+            {"config", required_argument, nullptr, 'c'},
+            {nullptr, 0, nullptr, 0} // End of options
+        };
 
-        args::variables_map vm;
-        args::store(args::parse_command_line(ac, av, desc), vm);
-        args::notify(vm);
-
-        if (vm.count("help")) {
-            cout << desc << "\n";
-            return EXIT_SUCCESS;
-        }
-
-        modmqttd::Log::severity level = modmqttd::Log::severity::info;
-        if (vm.count("loglevel")) {
-            level = (modmqttd::Log::severity)(vm["loglevel"].as<int>());
+        int opt;
+        while ((opt = getopt_long(ac, av, "hl:c:", cmdOptions, nullptr)) != -1) {
+            switch (opt) {
+                case 'h':
+                    std::cout << "Arguments:\n" << "  --help\t\tproduce help message\n" \
+                        << "  --loglevel arg\tset log level 1-6, higher is more verbose\n" \
+                        << "  --config arg\t\tpath to configuration file\n";
+                    return EXIT_SUCCESS;
+                case 'l':
+                    logLevel = std::stoi(optarg); // Convert string to int
+                    level = (modmqttd::Log::severity)(logLevel);
+                    if (logLevel < 0 || logLevel > 6) {
+                        std::cerr << "Log level must be between 0 and 6.\n";
+                        return 1;
+                    }
+                    break;
+                case 'c':
+                    configPath = optarg; // Get the config path
+                    break;
+                default:
+                    std::cerr << "Unknown option: " << char(optopt) << "\n";
+                    return 1;
+            }
         }
 
         modmqttd::Log::init_logging(level);
@@ -51,7 +62,7 @@ int main(int ac, char* av[]) {
         return EXIT_SUCCESS;
     } catch (const YAML::BadFile& ex) {
         if (configPath == "")
-            configPath = boost::filesystem::current_path().native();
+            configPath = std::filesystem::current_path().native();
         std::string msg = "Failed to load configuration from "s + configPath;
         spdlog::critical("{}", msg.c_str() );
     } catch (const std::exception& ex) {
